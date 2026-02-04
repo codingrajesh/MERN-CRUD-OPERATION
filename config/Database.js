08c9ac2cbeb30c7df3c6ab383a9272072b02aad1
Rajesh Gautam <irajgautam00@gmail.com>
2026-02-04 08:42:52 -0500

    Update MongoDB connection URI variable

--- config/Database.js
+++ config/Database.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 export const connectDB = async () => {
   try {
-    mongoose.connect(process.env.MONGO_URL)
+    await mongoose.connect(process.env.MONGO_URI)
     console.log('Connected to the Database successfully');
   } catch (error) {
     console.error('Could not connect to MongoDB', error);
